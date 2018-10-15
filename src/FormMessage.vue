f96c6ea9683b622f38c196c02d12e3cbfe298537
Nicolò Caruso <nicolo@istos.it>
2018-10-15 14:51:48 +0200

    Validate form in FormMessage before submit

--- src/FormMessage.vue
+++ src/FormMessage.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="sc-message--form">
     <div class="sc-message--form--text" v-html="data.text"></div>
-    <div v-for="(element, key) in data.elements" class="sc-message--form--element">
-      <span class="sc-message--form--element-label">{{ element }}:</span>
-      <input class="sc-message--form--element-input" v-model="form.data[key].value" />
+    <div v-if="errors.length" class="sc-message--form--errors">
+      <div v-for="error in errors">
+        {{ error }}
+      </div>
+    </div>
+    <div v-for="element in data.elements" class="sc-message--form--element">
+      <span class="sc-message--form--element-label">{{ element.display }}:</span>
+      <input class="sc-message--form--element-input" v-model="form.data[element.name].value" />
     </div>
     <button @click="_handleClick">{{ data.submit_text }}</button>
   </div>
@@ -33,20 +38,34 @@ export default {
     return {
       form: {
         data: []
-      }
+      },
+      errors: []
     }
   },
   methods: {
     _handleClick () {
-      this.onFormButtonClick(this.form.data, this.message)
+      this.validateForm();
+
+      if (!this.errors.length) {
+        this.onFormButtonClick(this.form.data, this.message)
+      }
+    },
+    validateForm () {
+      this.errors = [];
+
+      this.data.elements.forEach((element) => {
+        if (element.required && !this.form.data[element.name].value.length) {
+          this.errors.push('<em>' + element.display + '</em> field is required')
+        }
+      })
     }
   },
   created () {
     this.data.elements.forEach((element) => {
-      this.form.data.push({
-        label: element,
+      this.form.data[element.name] = {
+        name: element.name,
         value: ''
-      })
+      }
     })
   }
 }
@@ -59,7 +78,6 @@ export default {
   font-weight: 300;
   font-size: 14px;
   line-height: 1.4;
-  white-space: pre-wrap;
   word-wrap: break-word;
   max-width: 100%;
   -webkit-font-smoothing: subpixel-antialiased;
@@ -91,6 +109,10 @@ export default {
   margin-right: 40px;
 }
 
+.sc-message--form .sc-message--form--text {
+  margin-bottom: 10px;
+}
+
 .sc-message--form .sc-message--form--element {
   margin-bottom: 10px;
 }
@@ -100,5 +122,13 @@ export default {
 }
 .sc-message--form .sc-message--form--element-input {
   font-size: 13px;
+}
+
+.sc-message--form .sc-message--form--errors {
+  background: #f55555;
+  color: white;
+  padding: 2px 7px;
+  border-radius: 5px;
+  margin-bottom: 10px;
 }
 </style>
