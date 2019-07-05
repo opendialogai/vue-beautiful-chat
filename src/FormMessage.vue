d56dbd83c5a6602d1ae0b585d30faf45a3ed7921
Nicolò Caruso <nicolo@istos.it>
2019-07-05 11:49:18 +0200

    BRCB-20: Implement new form element of type “auto complete select”

--- src/FormMessage.vue
+++ src/FormMessage.vue
@@ -22,13 +22,22 @@
           </option>
         </select>
       </template>
+      <template v-if="element.element_type == 'auto-select'">
+        <v-select @input="onSelectChange" v-model="form.data[element.name].value" :options="element.options" :reduce="option => option.key" label="value"></v-select>
+      </template>
     </div>
     <button v-if="!data.auto_submit" @click="_handleClick">{{ data.submit_text }}</button>
   </div>
 </template>
 
 <script>
+import vSelect from 'vue-select'
+import 'vue-select/dist/vue-select.css'
+
 export default {
+  components: {
+    vSelect
+  },
   props: {
     data: {
       type: Object,
@@ -160,5 +169,11 @@ export default {
   padding: 2px 7px;
   border-radius: 5px;
   margin-bottom: 10px;
+}
+</style>
+
+<style>
+.sc-message--form--element .vs__dropdown-toggle {
+  background: white;
 }
 </style>
