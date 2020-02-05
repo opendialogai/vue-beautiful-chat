244e939f5ca9a64895ea333de61c81c383d1148e
Nicolò Caruso <nicolo@istos.it>
2020-02-05 12:01:16 +0100

    Add fade animation to the external buttons

--- src/ExternalButtons.vue
+++ src/ExternalButtons.vue
@@ -1,20 +1,22 @@
 <template>
   <div v-if="externalButtons.length" class="sc-external-buttons-row" :style="{background: colors.messageList.bg}">
-    <button
-      class="sc-external-buttons-element"
-      :class="(buttonClicked == idx) ? 'sc-external-buttons-element--clicked' : ''"
-      v-for="(externalButton, idx) in externalButtons"
-      v-on:click="_handleClick(externalButton, idx)"
-      :style="{background: colors.externalButton.bg, color: colors.externalButton.text, '--button-hover': colors.externalButton.hoverbg}"
-      :key="idx">
-      <div class="sc-external-buttons-element--top"></div>
-      <div class="sc-external-buttons-element--right"></div>
-      <div class="sc-external-buttons-element--bottom"></div>
-      <div class="sc-external-buttons-element--left"></div>
-      <div class="sc-external-buttons-element--background"></div>
+    <div class="sc-external-buttons-row-wrapper fade-enter-active">
+      <button
+        class="sc-external-buttons-element"
+        :class="(buttonClicked == idx) ? 'sc-external-buttons-element--clicked' : ''"
+        v-for="(externalButton, idx) in externalButtons"
+        v-on:click="_handleClick(externalButton, idx)"
+        :style="{background: colors.externalButton.bg, color: colors.externalButton.text, '--button-hover': colors.externalButton.hoverbg}"
+        :key="idx">
+        <div class="sc-external-buttons-element--top"></div>
+        <div class="sc-external-buttons-element--right"></div>
+        <div class="sc-external-buttons-element--bottom"></div>
+        <div class="sc-external-buttons-element--left"></div>
+        <div class="sc-external-buttons-element--background"></div>
 
-      <span v-html="externalButton.text"></span>
-    </button>
+        <span v-html="externalButton.text"></span>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -54,11 +56,13 @@ export default {
 <style scoped>
 .sc-external-buttons-row {
   text-align: center;
-  background: inherit;
   padding: 10px;
   overflow-x: scroll;
   overflow-y: hidden;
   white-space: nowrap;
+}
+.sc-external-buttons-row-wrapper {
+  animation-duration: 0s;
 }
 .sc-external-buttons-element {
   margin: 3px;
